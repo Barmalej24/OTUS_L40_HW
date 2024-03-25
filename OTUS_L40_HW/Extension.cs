15e3083d84e36b812ad642c7d4a95651035ea770
Jura <barmalej@live.ru>
2024-03-25 23:19:47 +0700

    Добавьте файлы проекта.

---
+++ OTUS_L40_HW/Extension.cs
@@ -0,0 +1,22 @@
+﻿namespace OTUS_L40_HW
+{
+    public static class Extension
+    {
+        public static IEnumerable<T> Top<T, Tkey>(this IEnumerable<T> list, int procent, Func<T, Tkey> selector)
+        {
+            if (procent < 1 || procent > 100)
+            {
+                throw new ArgumentException("Значения процентного соотношения выходит за предел от 0 до 100");
+            }
+
+            var countTakeItem = (int)Math.Ceiling(procent / (100.0 / list.Count()));
+
+            return list.OrderByDescending(selector).Take(countTakeItem);
+        }
+
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> list, int procent)
+        {
+            return list.Top(procent, x => x);
+        }
+    }
+}
---
+++ OTUS_L40_HW/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace OTUS_L40_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var list2 =  new List<string> { "q", "w", "e", "r" };
+            var list3 = new List<Person> {
+                    new Person() { Age = 82 },                   
+                    new Person() { Age = 26 },
+                    new Person() { Age = 57 },
+                    new Person() { Age = 34 },
+                    new Person() { Age = 67 },
+                    new Person() { Age = 10 },
+                    new Person() { Age = 77 },
+                    new Person() { Age = 70 }
+                };
+
+            Console.WriteLine("_________первый лист_________");
+            var value = list.Top(30);
+
+            Console.WriteLine($"Тим возвращаемого результата :: {value.GetType()}");
+
+            foreach (var item in value)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine("\n_____________________________");
+
+            Console.WriteLine("_________второй лист_________");
+            var value2 = list2.Top(40);
+
+            Console.WriteLine($"Тим возвращаемого результата :: {value2.GetType()}");
+
+            list2.Top(30).ToList().ForEach(x => Console.Write($"{x}\t"));
+
+            Console.WriteLine("\n_____________________________");
+
+            Console.WriteLine("_________третий лист_________");
+            var value3 = list3.Top(37, person => person.Age);
+
+            Console.WriteLine($"Тим возвращаемого результата :: {value3.GetType()}");
+
+            value3.ToList().ForEach(x => Console.Write($"{x.Age}\t"));
+
+            Console.WriteLine("\n_____________________________");
+        }
+
+        public class Person
+        {
+            public int Age { get; set; }
+        }
+    }
+}
